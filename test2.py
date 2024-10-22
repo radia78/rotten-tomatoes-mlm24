196e340e3aed50d4284fbb16dfd67b2a7e394e55
Radi Akbar <radia78@Radis-MacBook-Pro.local>
2024-10-22 14:07:12 -0500

    Cleaned house

--- test2.py
+++
@@ -1,4 +0,0 @@
-from utils.tools import load_and_decode, display_image_and_mask
-
-imgs, masks = load_and_decode("predictions/sample_submission.csv", img_dir="data/leaf_veins/test")
-display_image_and_mask(image=imgs[4], mask=masks[4], imgname="submission_results")
